450ab5833f9b6172e5fda18c982e287451b4d933
57853 <57853@DESKTOP-M1UEL66>
2017-12-04 17:09:51 +0800

    更新了需要的包

--- src/jsi/nova/gui/panel/GraphPanel.java
+++ src/jsi/nova/gui/panel/GraphPanel.java
@@ -8,6 +8,10 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+import com.mxgraph.swing.mxGraphComponent;
+
+import jsi.nova.util.ConstantsRepository;
+
 /**
  * @ClassName:     GraphPanel.java
  * @Description:   TODO(用一句话描述该文件做什么) 
@@ -21,6 +25,6 @@ public class GraphPanel extends JPanel{
     public GraphPanel() {
         // TODO Auto-generated constructor stub
         this.setLayout(new GridLayout(1, 1));
-        this.add(new JTextArea());
+        this.add(new mxGraphComponent(ConstantsRepository.graph));
     }
 }
---
+++ src/jsi/nova/util/ConstantsRepository.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package jsi.nova.util;
+
+import com.mxgraph.view.mxGraph;
+
+/**
+ * @ClassName:     ConstantsRepository.java
+ * @Description:   TODO(用一句话描述该文件做什么) 
+ * @author         ConstantsRepository
+ * @version        V1.0  
+ * @Date           2017年12月4日 下午3:53:35 
+ * @Place          北京航空航天大学中德软件联合研究所
+ */
+public class ConstantsRepository {
+    public static mxGraph graph = new mxGraph();
+
+}
