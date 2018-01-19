483b7938d4b775aa9e97f02137b50bf839c979f7
57853 <57853@DESKTOP-M1UEL66>
2018-01-19 13:39:53 +0800

    添加图片资源

--- src/jsi/nova/gui/projecttree/ProjectTreeMouseListener.java
+++ src/jsi/nova/gui/projecttree/ProjectTreeMouseListener.java
@@ -5,6 +5,7 @@ package jsi.nova.gui.projecttree;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
 
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -20,14 +21,14 @@ import jsi.nova.util.Constants;
  * @Date           2017年12月8日 下午2:45:39 
  * @Place          北京航空航天大学中德软件联合研究所
  */
-public class ProjectTreeMouseListener implements MouseListener{
-    
+public class ProjectTreeMouseListener implements MouseListener {
+
     private static JTree tree = ProjectTreePanel.getProject_tree();
 
     @Override
     public void mouseClicked(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -35,31 +36,34 @@ public class ProjectTreeMouseListener implements MouseListener{
         // TODO Auto-generated method stub
         DefaultMutableTreeNode dn = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
         //右键事件
-        if(e.getButton()==3){
-            if(dn instanceof ProjectTreeNode){
-                new ProjectPopMenu((ProjectTreeNode) dn).show(tree, e.getX(), e.getY());;
-            }
-            else if(dn instanceof GraphTreeNode){
-                new GraphPopMenu((GraphTreeNode) dn).show(tree, e.getX(), e.getY());;
+        if (e.getButton() == 3) {
+            if (dn instanceof ProjectTreeNode) {
+                new ProjectPopMenu((ProjectTreeNode) dn).show(tree, e.getX(), e.getY());
+                ;
+            } else if (dn instanceof GraphTreeNode) {
+                new GraphPopMenu((GraphTreeNode) dn).show(tree, e.getX(), e.getY());
+                ;
             }
         }
         //双击事件
-//        if(e.getClickCount()==2 && e.getButton()==1){
-//            if(dn instanceof GraphTreeNode){
-//                try {
-//                    GraphPopMenuActions.openGraphFile((GraphTreeNode) dn);
-//                } catch (FileNotFoundException e1) {
-//                    // TODO Auto-generated catch block
-//                    e1.printStackTrace();
-//                }
-//            }
-//        }
+        if (e.getClickCount() == 2 && e.getButton() == 1) {
+            if (dn instanceof GraphTreeNode) {
+                try {
+                    if (GraphPopMenuActions.ifSaved()) {
+                        GraphPopMenuActions.openGraphFile((GraphTreeNode) dn);
+                    }
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -71,7 +75,7 @@ public class ProjectTreeMouseListener implements MouseListener{
     @Override
     public void mouseExited(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
 }
